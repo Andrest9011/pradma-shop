ffbbd70d7069052745139714347b9476ff95e332
Sandra <sandra@MacBook-Pro-de-Sandra.local>
2020-10-20 11:12:21 -0500

    Diseno de componente products creado

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -13,7 +13,7 @@ export class DataService {
 
   getProducts(){
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoidXNlciIsImFwcElkIjoiYzE0YmE4OWQtNGU4YS00ZmZjLWJlOWMtNzdlOWEyMDc5MTRiIiwiZXhwIjoxNjAzMTM4OTQyLCJpc3MiOiJCbGl0eiIsImF1ZCI6Ind3dy5CbGl0ei5jb20ifQ.IjQQBtttmeH-1r4eh3tUe9PQjaF-336jwc3DH66m3qk'
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzaWQiOiIyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoidXNlciIsImFwcElkIjoiYzE0YmE4OWQtNGU4YS00ZmZjLWJlOWMtNzdlOWEyMDc5MTRiIiwiZXhwIjoxNjAzMjM0NzkwLCJpc3MiOiJCbGl0eiIsImF1ZCI6Ind3dy5CbGl0ei5jb20ifQ.coAzMNnDMyLN_Oj_dqVJ1IHaKoDQ51UFNZlR1tWo9NY'
     });
     return this.http.get('https://blitz-dev1.azurewebsites.net/ms-e-bill/api/product?page=1', {headers});
       
